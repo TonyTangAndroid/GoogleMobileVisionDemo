e255eca1fa51a5acfb58aee325c03f935587d966
ztang <ztang@microstrategy.com>
2016-12-12 17:47:31 -0500

    enable flash option

--- qrcodeDemo/src/main/java/com/google/android/gms/samples/vision/barcodereader/BarcodeCaptureActivity.java
+++ qrcodeDemo/src/main/java/com/google/android/gms/samples/vision/barcodereader/BarcodeCaptureActivity.java
@@ -20,11 +20,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -38,89 +38,41 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
-/**
- * Activity for the multi-tracker app.  This app detects barcodes and displays the value with the
- * rear facing camera. During detection overlay graphics are drawn to indicate the position,
- * size, and ID of each barcode.
- */
-public final class BarcodeCaptureActivity extends AppCompatActivity {
+public final class BarcodeCaptureActivity extends AppCompatActivity implements Detector.Processor<Barcode> {
     public static final String BarcodeObject = "Barcode";
     private static final String TAG = "Barcode-reader";
-    // intent request code to handle updating play services if needed.
     private static final int RC_HANDLE_GMS = 9001;
-    private CameraSource mCameraSource;
-    private CameraSourcePreview mPreview;
+    private CameraSource cameraSource;
+    private CameraSourcePreview cameraSourcePreview;
 
-    /**
-     * Initializes the UI and creates the detector pipeline.
-     */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.barcode_capture);
-
-        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
-        createCameraSource(true, false);
+        cameraSourcePreview = (CameraSourcePreview) findViewById(R.id.preview);
+        createCameraSource();
     }
 
 
-    private void createCameraSource(boolean autoFocus, boolean useFlash) {
+    @SuppressWarnings("deprecation")
+    private void createCameraSource() {
         Context context = getApplicationContext();
 
-        // A barcode detector is created to track barcodes.  An associated multi-processor instance
-        // is set to receive the barcode detection results, track the barcodes, and maintain
-        // graphics for each barcode on screen.  The factory is used by the multi-processor to
-        // create a separate tracker instance for each barcode.
         BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(context).build();
-
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-
-                final SparseArray<Barcode> detectedItems = detections.getDetectedItems();
-                if (detectedItems != null && detectedItems.size() > 0) {
-                    Intent data = new Intent();
-                    final Barcode value = detectedItems.valueAt(0);
-                    String rawValue = value.rawValue;
-                    Log.d("receiveDetections", "receiveDetections:" + rawValue);
-                    data.putExtra(BarcodeObject, value);
-                    setResult(CommonStatusCodes.SUCCESS, data);
-                    finish();
-                }
-            }
-        });
+        barcodeDetector.setProcessor(this);
 
 
         if (!barcodeDetector.isOperational()) {
-            // Note: The first time that an app using the barcode or face API is installed on a
-            // device, GMS will download a native libraries to the device in order to do detection.
-            // Usually this completes before the app is run for the first time.  But if that
-            // download has not yet completed, then the above call will not detect any barcodes
-            // and/or faces.
-            //
-            // isOperational() can be used to check if the required native libraries are currently
-            // available.  The detectors will automatically become operational once the library
-            // downloads complete on device.
             Log.w(TAG, "Detector dependencies are not yet available.");
-
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-
+            IntentFilter lowStorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowStorageFilter) != null;
             if (hasLowStorage) {
                 Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
                 Log.w(TAG, getString(R.string.low_storage_error));
             }
         }
 
-        // Creates and starts the camera.  Note that this uses a higher resolution in comparison
+        // This uses a higher resolution in comparison
         // to other detection examples to enable the barcode detector to detect small barcodes
         // at long distances.
         CameraSource.Builder builder = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
@@ -129,19 +81,23 @@ public final class BarcodeCaptureActivity extends AppCompatActivity {
                 .setRequestedFps(15.0f);
 
         // make sure that auto focus is an available option
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            builder = builder.setFocusMode(
-                    autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null);
-        }
-
-        mCameraSource = builder
-                .setFlashMode(useFlash ? Camera.Parameters.FLASH_MODE_TORCH : null)
-                .build();
+        cameraSource = builder.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE).build();
     }
 
-    /**
-     * Restarts the camera.
-     */
+    private void onItemDetected(Detector.Detections<Barcode> detections) {
+        final SparseArray<Barcode> detectedItems = detections.getDetectedItems();
+        if (detectedItems != null && detectedItems.size() > 0) {
+            final Barcode value = detectedItems.valueAt(0);
+            String rawValue = value.rawValue;
+            Log.d("receiveDetections", "receiveDetections:" + rawValue);
+            Intent data = new Intent();
+            data.putExtra(BarcodeObject, value);
+            setResult(CommonStatusCodes.SUCCESS, data);
+            finish();
+        }
+    }
+
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -154,8 +110,8 @@ public final class BarcodeCaptureActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if (mPreview != null) {
-            mPreview.stop();
+        if (cameraSourcePreview != null) {
+            cameraSourcePreview.stop();
         }
     }
 
@@ -166,8 +122,8 @@ public final class BarcodeCaptureActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mPreview != null) {
-            mPreview.release();
+        if (cameraSourcePreview != null) {
+            cameraSourcePreview.release();
         }
     }
 
@@ -187,16 +143,36 @@ public final class BarcodeCaptureActivity extends AppCompatActivity {
             dlg.show();
         }
 
-        if (mCameraSource != null) {
+        if (cameraSource != null) {
             try {
-                mPreview.start(mCameraSource);
+                cameraSourcePreview.start(cameraSource);
             } catch (IOException e) {
                 Log.e(TAG, "Unable to start camera source.", e);
-                mCameraSource.release();
-                mCameraSource = null;
+                cameraSource.release();
+                cameraSource = null;
             }
         }
     }
 
 
+    @SuppressWarnings("deprecation")
+    public void switchFlash(View view) {
+
+        if (Camera.Parameters.FLASH_MODE_TORCH.equals(cameraSource.getFlashMode())) {
+            cameraSource.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        } else {
+            cameraSource.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+
+        }
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<Barcode> detections) {
+        onItemDetected(detections);
+    }
 }
